402737cd8163627eab15dbb5dab7bb2e3a05a91d
Gui_T <guilherme.gui.tama@gmail.com>
2022-08-22 14:47:26 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<script >
+export default {
+  data() {
+    return {
+      accountValue: 0,
+      Tip: 15,
+      peopleNumber: 1,
+    };
+  },
+  methods: {
+    calcular() {
+      const accountValue = this.accountValue;
+
+      const Tip = this.Tip;
+
+      const peopleNumber = this.peopleNumber;
+
+      let valueTip = 0;
+
+      let valueTotal = {};
+
+      valueTip = accountValue * (Tip / 100);
+
+      valueTotal = (accountValue + valueTip) / peopleNumber;
+
+      valueTotal = Math.round(valueTotal * 100) / 100;
+
+      valueTotal = valueTotal.toFixed(2);
+
+      console.log(valueTotal);
+
+      document.getElementById("resultado").innerHTML = valueTotal;
+    }
+  }
+  //K K K K K K K K K K K K K K K K K K K K K K KK K K K K K K K K K K K K K K K K K K K K K K KK K K KK  K KK K K KK K KK K K KK K KK K KK K K KK K KK K  KK K KK K K KK K KK K KK K  KK K KK 
+};
+</script>
+
+<template>
+
+  <div class="main">
+
+    <h1><strong>Conta</strong></h1>
+
+    <form>
+
+      <label>
+        <p>Qual o preço do jantar?</p>
+        <input type="number" v-model="accountValue" id="accountValue">
+      </label>
+
+      <label>
+        <p>Qual a Gorjeta? </p>
+        <select v-model="Tip" id="valueTip">
+          <option value="10000">Mundo das ideias</option>
+          <option value="30">30% Mas bah </option>
+          <option value="25">25% Muito bom</option>
+          <option value="20">20% Bom</option>
+          <option value="15">15% Aceitável</option>
+          <option value="10">10% Ruim</option>
+          <option value="5">5% "Nojo -_-"</option>
+        </select>
+      </label>
+
+      <label>
+        <p>Quantas pessoas vão dividir a Conta?</p>
+        <input v-model="peopleNumber" type="number" id="peopleNumber" />
+      </label>
+
+      <label>
+        <button type="button" @click="calcular">CALCULAR</button>
+      </label>
+      <label >
+        <p>Valor</p>
+        <span id="resultado">R$ 0.00</span>
+      </label>
+
+    </form>
+
+  </div>
+
+</template>
+
+<style scoped>
+*{
+background-color: black;
+color: white;
+}
+button {
+  border: 1px solid red;
+}
+span {
+  border: 1px solid red;
+}
+select {
+  border:1px solid red;
+}
+div {
+  text-align: center;
+}
+input {
+  border: 1px solid red;
+}
+form label {
+  margin: 20px;
+  display: block;
+}
+form {
+  width: 1500px;
+  height: 1000px;
+}
+h1 {
+  padding-right:80px ;
+}
+span {
+  font-size:20px;
+  padding: 10px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+  <div class="calculadora">
+  <h3> Calculadora de gorjetas </h3>
+  <div class="valor">Valor da conta
+    <div><input type="number"></div>
+  </div>
+  <div class="valor">Como foi nosso atendimento?
+    <div><select name="15%(OK)" id=""> 
+      <option value="30">30%(Excepcional)</option>
+      <option value="25">25%(Mais do que bom)</option>
+      <option value="20">20%(Bom)</option>
+      <option value="15">15%(OK)</option>
+      <option value="10">10%(ruin)</option>
+      <option value="5">5%(Péssimo)</option>
+      
+      </select></div>
+  </div>
+  </div>
+</template>
+<style>
+#valor {
+  margin: 15px;
+}
+.calculadora {
+  justify-content: center;
+  text-align: center;
+  padding-top:100px ;
+}
+select {
+  height: 30px;
+  width: 256px;
+}
+</style>
